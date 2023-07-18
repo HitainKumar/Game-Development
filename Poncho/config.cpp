4d35dc53bf958c6a32def9b2d470ff5cbcccdf81
Hitain Kumar <136987557+HitainKumar@users.noreply.github.com>
2023-07-18 23:38:03 +0530

    Camonets

---
+++ Poncho/config.cpp
@@ -0,0 +1,393 @@
+class CfgPatches
+{
+	class TeddyCamoNetPoncho
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Data"
+		};
+	};
+};
+class CfgVehicles
+{
+	class INK_CamoNetPoncho_Base;
+	class Teddy_VIP6_CamonetFlares: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - VIP6 Net Poncho";
+		hiddenSelections[]={"color"};
+		varWetMax=0.249;
+		heatIsolation=1;
+		biological=1;
+		chemical=1;
+		itemsCargoSize[]={10,3};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_VIP_Poncho1.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_VIP_Poncho1.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_VIP_Poncho1.paa"
+		};
+	};
+	class Teddy_VIP5_CamonetTitan: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - VIP5 Net Poncho";
+		hiddenSelections[]={"color"};
+		varWetMax=0.249;
+		heatIsolation=1;
+		biological=1;
+		chemical=1;
+		itemsCargoSize[]={10,3};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Titan.paa",
+			"Teddy_Clothings\Poncho\data\Titan.paa",
+			"Teddy_Clothings\Poncho\data\Titan.paa"
+		};
+	};
+	class Teddy_VIP4_CamonetBlue: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - VIP4 Camo Net Poncho";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Blue.paa",
+			"Teddy_Clothings\Poncho\data\Blue.paa",
+			"Teddy_Clothings\Poncho\data\Blue.paa"
+		};
+	};
+    class Teddy_VIP3_CamonetHyperBeast: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - VIP3 HyperBeast Camo Net Poncho";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\HyperBeast.paa",
+			"Teddy_Clothings\Poncho\data\HyperBeast.paa",
+			"Teddy_Clothings\Poncho\data\HyperBeast.paa"
+		};
+	};
+    class Teddy_VIP2_CamonetRed: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - VIP2 Red Camo Net Poncho";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Red.paa",
+			"Teddy_Clothings\Poncho\data\Red.paa",
+			"Teddy_Clothings\Poncho\data\Red.paa"
+		};
+	};
+    class Teddy_VIP1_CamonetSkin: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - VIP1 Skin Camo Net Poncho";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Skin.paa",
+			"Teddy_Clothings\Poncho\data\Skin.paa",
+			"Teddy_Clothings\Poncho\data\Skin.paa"
+		};
+	};
+	class Teddy_CamonetAdminTitanRp: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - Admin Camo Net Poncho";
+		hiddenSelections[]={"color"};
+		varWetMax=0.249;
+		heatIsolation=1;
+		biological=1;
+		chemical=1;
+		itemsCargoSize[]={10,20};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\admin.paa",
+			"Teddy_Clothings\Poncho\data\admin.paa",
+			"Teddy_Clothings\Poncho\data\admin.paa"
+		};
+	};
+	class Teddy_CamonetOwnerTitanRp: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - magic Camo Net Poncho";
+		hiddenSelections[]={"color"};
+		varWetMax=0.249;
+		heatIsolation=1;
+		biological=1;
+		chemical=1;
+		itemsCargoSize[]={10,30};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\magic_owner.paa",
+			"Teddy_Clothings\Poncho\data\magic_owner.paa",
+			"Teddy_Clothings\Poncho\data\magic_owner.paa"
+		};
+	};
+	class Teddy_CamonetPurpleTitanRp: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - Purple Camo Net Poncho";
+		hiddenSelections[]={"color"};
+		varWetMax=0.249;
+		heatIsolation=1;
+		biological=1;
+		chemical=1;
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_PurplePoncho.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_PurplePoncho.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_PurplePoncho.paa"
+		};
+	};
+	class Teddy_CamonetBlueTitanRp: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - Blue Camo Net Poncho";
+		hiddenSelections[]={"color"};
+		varWetMax=0.249;
+		heatIsolation=1;
+		biological=1;
+		chemical=1;
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_BluePoncho.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_BluePoncho.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_BluePoncho.paa"
+		};
+	};
+	class Teddy_CamonetYellowTitanRp: INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="TitanRP & Teddy - Yellow Camo Net Poncho";
+		hiddenSelections[]={"color"};
+		varWetMax=0.249;
+		heatIsolation=1;
+		biological=1;
+		chemical=1;
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_YellowPoncho.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_YellowPoncho.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_YellowPoncho.paa"
+		};
+	};
+	class Teddy_Camoflauge_Camonet : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Camoflauge camonet";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_Camoflauge_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Camoflauge_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Camoflauge_Camonet.paa"
+		};
+	};
+	class Teddy_Camoflauge_Camonet2 : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Camoflauge2 camonet";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_Camoflauge_Camonet2.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Camoflauge_Camonet2.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Camoflauge_Camonet2.paa"
+		};
+	};
+	class Teddy_Damascus_Camonet : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Damascus camonet";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_Damascus_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Damascus_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Damascus_Camonet.paa"
+		};
+	};
+	class Teddy_Lava_Camonet : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Lava camonet";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_Lava_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Lava_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Lava_Camonet.paa"
+		};
+	};
+	class Teddy_LavaBlue_Camonet : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Lava Blue camonet";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_LavaBlue_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_LavaBlue_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_LavaBlue_Camonet.paa"
+		};
+	};
+	class Teddy_LavaGreen_Camonet : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Lava Green camonet";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_LavaGreen_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_LavaGreen_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_LavaGreen_Camonet.paa"
+		};
+	};
+	class Teddy_LavaPink_Camonet : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Lava Pink camonet";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_LavaPink_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_LavaPink_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_LavaPink_Camonet.paa"
+		};
+	};
+	class Teddy_Pubg_Camonet : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Pubg camonet";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_Pubg_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Pubg_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_Pubg_Camonet.paa"
+		};
+	};
+	class Teddy_T_Camonet : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Anime camonet";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_T_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_T_Camonet.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_T_Camonet.paa"
+		};
+	};
+	class Teddy_new_poncho1 : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Full camonet 1";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho1.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho1.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho1.paa"
+		};
+	};
+	class Teddy_new_poncho2 : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Full camonet 2";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho2.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho2.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho2.paa"
+		};
+	};
+	class Teddy_new_poncho3 : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Full camonet 3";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho3.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho3.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho3.paa"
+		};
+	};
+	class Teddy_new_poncho4 : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Full camonet 4";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho4.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho4.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho4.paa"
+		};
+	};
+	class Teddy_new_poncho5 : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Full camonet 5";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho5.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho5.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho5.paa"
+		};
+	};
+	class Teddy_new_poncho6 : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Full camonet 5";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho6.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho6.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho6.paa"
+		};
+	};
+	class Teddy_new_poncho7 : INK_CamoNetPoncho_Base
+	{
+		scope=2;
+		displayName="Teddy - Full camonet 7";
+		hiddenSelections[]={"color"};
+		itemsCargoSize[]={10,2};
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho7.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho7.paa",
+			"Teddy_Clothings\Poncho\data\Teddy_new_poncho7.paa"
+		};
+	};
+};
