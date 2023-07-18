d6a070bf470e804a16456deb00ab57e2b94be5c5
Hitain Kumar <136987557+HitainKumar@users.noreply.github.com>
2023-07-18 23:34:45 +0530

    BBP-wallpapers

---
+++ wallpaper_mural/config.cpp
@@ -0,0 +1,399 @@
+class CfgPatches
+{
+	class Teddy_wallpapers
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={"RPC_Scripts","DZ_Characters","DZ_Characters_Backpacks","DZ_Characters_Pants","DZ_Characters_Tops","DZ_Gear_Containers","DZ_Characters_Headgear","DZ_Gear_Camping","DZ_Gear_Crafting","DZ_Gear_Tools","DZ_Animals","DZ_Data"};
+	};
+};
+
+class CfgVehicles
+{
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_1: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 1 : PURGE ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_1.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_2: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 2 : Call Of Duty";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_2.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_3: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 3 : Call Of Duty 1";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_3.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_4: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 4 : Tom Calancy ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_4.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_5: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 5 : Call Of Duty 2 ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_5.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_6: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 6 : Call Of Duty 3 ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_6.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_7: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 7 : Sonic  ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_7.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_8: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 8 : Call Of Duty 4 ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_8.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_9: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 9 : Call Of Duty 5 ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_9.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_10: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 10 : Free Fire ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_10.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_LunnaShop: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 10 : Free Fire ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_LunnaShop.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_Poppy: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 11 : Poppy ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Poppy.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Poppy_Trader: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 12 : Poppy's Trader ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Poppy_Trader.paa"
+		};
+	};
+    class BBP_WallPaper_Mural;
+	class Teddy_Mural_Gunsmith: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 13 : Gunsmith ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Gunsmith.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Gunsmith_Extend: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 14 : Gunsmith Extend";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Gunsmith_Extend.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Lunna_Extend: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 15 : Lunna Extend ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Lunna_Extend.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Lunna_Extend_1: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 16 : Lunna Extend 1 ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Lunna_extend_1.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_11: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 17 : DAYZ  ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_11.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_12: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 18 : Sigma  ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_12.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_13: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 19 : Universe  ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_13.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_14: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 20 : Blood  ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_14.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_15: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 21 :  180SX ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_15.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_16: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 22 : Green Verse  ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_16.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_17: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 23 : Red Verse  ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_17.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_18: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 24 : Purple Verse ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_18.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_19: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 25 : Black Verse ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_19.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_20: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 26 :  Red Zap  ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_20.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_21: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 27 :  Batman ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Batman.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Earth: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 27 :  Teddy_Mural_Earth ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Earth.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Plane: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 28 :  Teddy_Mural_Plane ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Plane.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_pubg: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 29 :  Teddy_Mural_pubg ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_pubg.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Ship: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 30 :  Teddy_Mural_Ship ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Ship.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Space: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 31 :  Teddy_Mural_Space ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Space.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Space2: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 32 :  Teddy_Mural_Space2 ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Space2.paa"
+		};
+	};
+	class BBP_WallPaper_Mural;
+	class Teddy_Mural_Vikings: BBP_WallPaper_Mural
+	{
+		scope=2;
+		displayName="Teddy's Clothings Mural 33 :  Teddy_Mural_Vikings ";
+		descriptionShort="Teddys Clothing Pack. Get Your Custom Retextures done , contact me on Titan RP's discord - https://discord.gg/KcfpT4mbvq ";
+		hiddenSelectionsTextures[]=
+		{
+			"Teddy_Clothings\wallpaper_mural\data\Teddy_Mural_Vikings.paa"
+		};
+	};
+}; 
